bb4a89ea15f01954a9fc0ecfd09580bdc58069ea
giuliom <lefantome@alice.it>
2011-11-26 20:39:22 +0000

    giulio's first commit

---
+++ Cookbook/src/com/cookbook/CookbookActivity.java
@@ -0,0 +1,17 @@
+package com.cookbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+//Giulio's first commit
+
+
+public class CookbookActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
