17c1ae44b56746162770c06e654d490d9bdabb1d
giuliom <lefantome@alice.it>
2011-11-28 22:16:53 +0000

    recipes input from text file working

--- gen/com/cookbook/R.java
+++ gen/com/cookbook/R.java
@@ -22,14 +22,18 @@ public final class R {
         public static final int sample_7=0x7f020008;
     }
     public static final class id {
-        public static final int gridview=0x7f050000;
+        public static final int gridview=0x7f060000;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
         public static final int main=0x7f030001;
+        public static final int recipe_page=0x7f030002;
+    }
+    public static final class raw {
+        public static final int recipes=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
--- src/com/cookbook/CookbookActivity.java
+++ src/com/cookbook/CookbookActivity.java
@@ -1,10 +1,18 @@
 package com.cookbook;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import com.cookbook.core.Recipe;
 import com.cookbook.core.RecipeList;
+import com.cookbook.core.readFile;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,32 +22,53 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 
 public class CookbookActivity extends ListActivity {
-    /** Called when the activity is first created. */
+    
+	
+	protected TextView textView;
+    readFile rd;
+    InputStream fos;
+    String FILENAME = "./res/raw/recipes";
+    Resources myResources;
+    static final RecipeList list = new RecipeList();
+    
+	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
 
+	  myResources = getResources();
+	 
+	
+
+		fos = myResources.openRawResource(R.raw.recipes);
 	  
-	  list.addRecipe(new Recipe("pizza","flour, tomato","bake everything", 1,Recipe.TypeOfMeal.MAIN,30,
+		rd = new readFile(fos,list);
+	  rd.read();
+		
+	 /* list.addRecipe(new Recipe("pizza","flour, tomato","bake everything", 1,Recipe.TypeOfMeal.MAIN,30,
 			  Recipe.Season.SPRING,"Italia",5f));
 	  list.addRecipe(new Recipe("hamburger","bread, meat","bake everything", 1,Recipe.TypeOfMeal.DESSERT,30,
 			  Recipe.Season.NULL,"Usa",5f));
 	  list.addRecipe(new Recipe("kebab","bread, lupus","bake everything", 1,Recipe.TypeOfMeal.SECOND,10,
-			  Recipe.Season.NULL,"turkey",4f));
-	  RECIPES = new String[] {list.getRecipe(0).getName(),
-				list.getRecipe(1).getName(), list.getRecipe(2).getName()};
+			  Recipe.Season.NULL,"turkey",4f));*/
+	  
+	  RECIPES = new String[] {list.getRecipe(0).getName()+"\nType: "+list.getRecipe(0).getType(),
+				list.getRecipe(1).getName()+"\nType: "+list.getRecipe(1).getType(),list.getRecipe(2).getName()+ "\nType: "+list.getRecipe(2).getType()};
 	  
 	  setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, RECIPES));
-
-	  ListView lv = getListView();
+	  
+	 
+	  
+	  final ListView lv = getListView();
 	  lv.setTextFilterEnabled(true);
-
+	  
 	  lv.setOnItemClickListener(new OnItemClickListener() {
 	    public void onItemClick(AdapterView<?> parent, View view,
 	        int position, long id) {
@@ -48,11 +77,16 @@ public class CookbookActivity extends ListActivity {
 	    "Ingredients: "+list.getRecipe(position).getIngredients()+"\nPreparation: "+list.getRecipe(position).getPreparation()
 	    +"\nType: "+list.getRecipe(position).getType(),
 	          Toast.LENGTH_SHORT).show();
+	    	
+	    
+	    
+	    
+	   
 	    }
 	  });
 	}
 	
-	static final RecipeList list = new RecipeList();
+	
 	 String[] RECIPES = new String[]{"lol"};
 	
 	
