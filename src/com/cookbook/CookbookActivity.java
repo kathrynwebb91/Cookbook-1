aa94e39c03dd801991ff581afb38b4b11d6372f4
Kathryn Webb <kathrynwebb.91@gmail.com>
2011-12-03 00:00:21 +0000

    Working tabs

--- src/com/cookbook/CookbookActivity.java
+++ src/com/cookbook/CookbookActivity.java
@@ -11,7 +11,9 @@ import com.cookbook.core.readFile;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.app.TabActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
@@ -23,11 +25,47 @@ import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+public class CookbookActivity extends TabActivity {
+    @Override	
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+	    
+	    Resources res = getResources(); // Resource object to get Drawables
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+	    Intent intent;  // Reusable Intent for each tab
 
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, MyRecipesActivity.class);
+
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabHost.newTabSpec("recipes").setIndicator("My Recipes",
+	                      res.getDrawable(R.drawable.star))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    // Do the same for the other tabs
+	    intent = new Intent().setClass(this, BookmarkActivity.class);
+	    spec = tabHost.newTabSpec("bookmarks").setIndicator("Bookmarks",
+	                      res.getDrawable(R.drawable.star))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    tabHost.setCurrentTab(1);
+	    
+	    
+	}
+	    
+}
+
+
+/*
 public class CookbookActivity extends ListActivity {
     
 	
@@ -39,25 +77,25 @@ public class CookbookActivity extends ListActivity {
     RecipeList list;
     
 	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+//    @Override
+/*	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
 
 	  myResources = getResources();
 	 
-	
+*/	
 	  /**
 	   * Opening the file in the project folder "res/raw/recipes" and generating the input stream
 	   */
-		fos = myResources.openRawResource(R.raw.recipes);
+/*		fos = myResources.openRawResource(R.raw.recipes);
 		rd = new readFile();
 	  list = rd.read(fos);
 		
-	 
+	*/ 
 	  /**
 	   * adding the list to the recipeArray used to display it
 	   */
-	  RECIPES = new String[list.size()];
+/*	  RECIPES = new String[list.size()];
 	  for (int i =0; i<list.size();i++){
 		  RECIPES[i] = list.getRecipe(i).getName()+"\nType: "+list.getRecipe(i).getType();
 	  }
@@ -71,11 +109,11 @@ public class CookbookActivity extends ListActivity {
 	  final ListView lv = getListView();
 	  lv.setTextFilterEnabled(true);
 	  
-	  
+	*/  
 	  /**
 	   * Onclik show the info about the Recipe on a popup message
 	   */
-	  lv.setOnItemClickListener(new OnItemClickListener() {
+/*	  lv.setOnItemClickListener(new OnItemClickListener() {
 	    public void onItemClick(AdapterView<?> parent, View view,
 	        int position, long id) {
 	      // When clicked, show a toast with the TextView text
@@ -91,11 +129,11 @@ public class CookbookActivity extends ListActivity {
 	    }
 	  });
 	}
-	
+*/	
 	/** need it*/
-	 String[] RECIPES = new String[]{"lol"};
+/*	 String[] RECIPES = new String[]{"lol"};
 	
 	
 	
 	
-}
+}*/
